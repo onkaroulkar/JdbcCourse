561dcf037c20ae22c385e20d4f5fb02cd417c0ac
onkar oulkar <onkaroulkar@gmail.com>
2025-03-07 13:22:53 +0530

    new commit

--- src/com/onkar/jdbc/connection/DemoJdbc.java
+++ src/com/onkar/jdbc/connection/DemoJdbc.java
@@ -13,8 +13,17 @@ public class DemoJdbc {
 		String url = "jdbc:mysql://localhost:3306/imdb";
 		String uname = "root";
 		String pass = "root";
+		String str = "select * from actors";
 		Connection con = DriverManager.getConnection(url, uname, pass);
 		System.out.println("connection established");
+		Statement st = con.createStatement();
+		  
+		/*
+		 * while(rs.next()){ System.out.println(rs.getString("first_name")); };
+		 */
+		//System.out.println(rs.getString("first_name"));
+		con.close();
+		System.out.println("connection closed");
 	}
 
 }
